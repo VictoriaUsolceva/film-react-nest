ec5e615fbf1fc1cb12101647f429c847095f8b63
VictoriaUsolceva <nikasidorova039@gmail.com>
2026-09-15 19:34:21 +0300

    fix: app provider use ConfigService

--- backend/src/app.config.provider.ts
+++ backend/src/app.config.provider.ts
@@ -1,14 +1,20 @@
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+export const CONFIG = 'CONFIG';
+
+const configService = new ConfigService();
+const configDatabase: AppConfigDatabase = {
+  driver: configService.get<string>('DATABASE_DRIVER', 'mongodb'),
+  url: configService.get<string>(
+    'DATABASE_URL',
+    'mongodb://localhost:27017/afisha',
+  ),
+};
 
 export const configProvider = {
   imports: [ConfigModule.forRoot()],
-  provide: 'CONFIG',
-  useValue: <AppConfig>{
-    database: {
-      driver: process.env.DATABASE_DRIVER,
-      url: process.env.DATABASE_URL,
-    },
-  },
+  provide: CONFIG,
+  useValue: <AppConfig>{ database: configDatabase },
 };
 
 export interface AppConfig {
