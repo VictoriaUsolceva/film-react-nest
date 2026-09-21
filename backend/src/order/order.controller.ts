20a6451266ef5e10eef3e52b2846a8b8d4ff2d74
VictoriaUsolceva <nikasidorova039@gmail.com>
2026-09-21 18:00:32 +0300

    fix: change response code to 200 per docs

--- backend/src/order/order.controller.ts
+++ backend/src/order/order.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseFilters } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, UseFilters } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { CreateOrderDto } from './dto/order.dto';
 import { BadRequestExceptionFilter } from 'src/filters/bad-request-exception.filter';
@@ -8,6 +8,7 @@ import { BadRequestExceptionFilter } from 'src/filters/bad-request-exception.fil
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
+  @HttpCode(200)
   @Post()
   async create(@Body() orders: CreateOrderDto) {
     return await this.orderService.create(orders);
